ee41ee1ad61db7df254ef3bc70a4b0d1f057e0a3
vishal lokhande <lvishal108@gmail.com>
2025-06-19 09:53:45 +0000

    basic ui part done with home page , views

--- NewsProject/news/newsapp/views.py
+++ NewsProject/news/newsapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 def news(request):
-    return HttpResponse("News")
+    name = "News App"
+    feature1 = "Live news by category"
+    feature2 = "Responsive UI with Bootstrap"
+    feature3 = "Easy navigation with buttons"
+    techstack = "Django, Python, Bootstrap"
+    deployoptions = "Vercel", "Cloudflare", "Netlify"
+
+    my_dict = {
+        'name': name,
+        'feature1': feature1,
+        'feature2': feature2,
+        'feature3': feature3,
+        'techstack': techstack,
+        'deployoptions': deployoptions
+    }
+
+    return render(request, 'newsapp/home.html', context=my_dict)
